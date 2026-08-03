2d73326668959b7706c14b2db66837c44f851a86
PowerTeamMessina <piscinanizzadisicilia@gmail.com>
2026-08-03 10:04:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2247,7 +2247,6 @@ ripristino_iniziale_da_github()
 backup_giornaliero_github()
 
 st.title("🏊 Statino Presenze Corsi di Nuoto")
-st.error("VERSIONE TEST 03/08/2026")
 
 stagioni = get_stagioni()
 
