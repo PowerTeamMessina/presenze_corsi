53029d9eda9124e93303c6ab2dc09b821f65ed93
PowerTeamMessina <piscinanizzadisicilia@gmail.com>
2026-08-06 15:54:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,11 @@ from docx import Document
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+
+from PyPDF2 import PdfReader, PdfWriter
+
 
 # ============================================================
 # CONFIGURAZIONE
