0a04ebde20fed96c0b351cfcdc8ae98bbc6ac752
PowerTeamMessina <piscinanizzadisicilia@gmail.com>
2026-08-07 00:02:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3428,7 +3428,7 @@ def genera_pdf_compilato(
         c,
         f"{valore_pdf(b['nome'])} {valore_pdf(b['cognome'])}",
         180,
-        700
+        690
     )
     
     scrivi_centrato(
@@ -3437,7 +3437,7 @@ def genera_pdf_compilato(
             s.get("luogo_nascita_bambino", "")
         ),
         470,
-        700
+        690
     )
     
     scrivi_centrato(
@@ -3446,7 +3446,7 @@ def genera_pdf_compilato(
             s.get("data_nascita_bambino", "")
         ),
         90,
-        670
+        675
     )
     
     scrivi_centrato(
@@ -3455,7 +3455,7 @@ def genera_pdf_compilato(
             s.get("cf_bambino", "")
         ),
         500,
-        670
+        675
     )
     
     scrivi_centrato(
