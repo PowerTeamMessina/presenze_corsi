63eec843721702fc05bbba61bbc8683d555bd838
PowerTeamMessina <piscinanizzadisicilia@gmail.com>
2026-08-06 11:17:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6252,22 +6252,20 @@ if is_genitore():
 
             valore_data = date(1990, 1, 1)
     
-            if not scheda.empty and valori.get("data_nascita_genitore"):
+            valore_data = datetime.strptime(
+            valori["data_nascita_genitore"],
+                "%Y-%m-%d"
+            ).date()
                         
-                valore_data = datetime.strptime(
-                valori["data_nascita_genitore"],
-                    "%Y-%m-%d"
-                ).date()
-                        
-                data_nascita_genitore = st.date_input(
-                    "Data di nascita del genitore",
-                    value=valore_data,
-                    min_value=date(1940, 1, 1),
-                    max_value=date.today(),
-                    disabled=bloccato,
-                    format="DD/MM/YYYY",
-                    key=f"data_nascita_bambino_{bambino_id}"
-                )
+            data_nascita_genitore = st.date_input(
+                "Data di nascita del genitore",
+                value=valore_data,
+                min_value=date(1940, 1, 1),
+                max_value=date.today(),
+                disabled=bloccato,
+                format="DD/MM/YYYY",
+                key=f"data_nascita_bambino_{bambino_id}"
+            )
         
         col1, col2 = st.columns(2)
         
